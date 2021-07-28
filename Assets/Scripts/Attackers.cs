fe63060f5fddbad1a42c359c7e8f960d17c01cb2
BhumeshwariKhotele <bhumeshwari.khotele@xcubelabs.com>
2021-07-28 17:04:38 +0530

    changed the platform to android

--- Assets/Scripts/Attackers.cs
+++ Assets/Scripts/Attackers.cs
@@ -7,27 +7,40 @@ public class Attackers : MonoBehaviour
     // Start is called before the first frame update
     [Range(-1f, 2f)]
     public float lizardWalkSpeed;
-    Animator lizardAnim; 
-
+    Animator lizardAnim;
+    SpriteRenderer lizardSprite;
     void Start()
     {
         lizardAnim = GetComponent<Animator>();
+     //  Rigidbody2D rb =AddComponent<Rigidbody2D>();
+        lizardSprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-        if(lizardWalkSpeed > 0 && lizardWalkSpeed <= 2f)
+        if (lizardWalkSpeed > 0)
         {
+            lizardSprite.flipX = false;
+            lizardAnim.SetFloat("IsWalking", lizardWalkSpeed);
+            transform.Translate(Vector3.left * lizardWalkSpeed * Time.deltaTime);
+
+        }
+
+
+        if (lizardWalkSpeed < 0)
+        {
+            lizardSprite.flipX = true;
             lizardAnim.SetFloat("IsWalking", lizardWalkSpeed);
             transform.Translate(Vector3.left * lizardWalkSpeed * Time.deltaTime);
           
         }
 
-        if (lizardWalkSpeed == 0 )
-        {
-           
-        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log( "triggered with" +name );
     }
 }
